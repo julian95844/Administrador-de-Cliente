ababa1e74dccc5c68bba77500c38507da1a272d7
julian95844 <julian95844@hotmail.com>
2023-09-12 12:33:16 -0300

    feat: mas estilos a los links y aprendiendo loader

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import Layout from './components/Layout'
 import NuevoCliente from './pages/NuevoCliente'
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
-import Index from "./pages/Index"
+import Index, { loader as clientesLoader} from "./pages/Index"
 
 const router = createBrowserRouter([
   {
@@ -13,7 +13,8 @@ const router = createBrowserRouter([
     children: [
       {
         index: true,
-        element: <Index />
+        element: <Index />,
+        loader: clientesLoader
       },
       {
           path: "/clientes/nuevo",
