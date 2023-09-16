09d1ff4741a8a5a0c569c9079b2ebc283d527267
julian95844 <julian95844@hotmail.com>
2023-09-15 23:52:51 -0300

    feat: añadiendo validacion y estilo al formulario

---
+++ src/components/Error.jsx
@@ -0,0 +1,10 @@
+
+function Error({children}) {
+  return (
+    <div className="text-center my-4 bg-red-600 p-3 uppercase text-white font-bold">
+      {children}
+    </div>
+  )
+}
+
+export default Error
--- src/pages/NuevoCliente.jsx
+++ src/pages/NuevoCliente.jsx
@@ -1,14 +1,33 @@
-import { useNavigate, Form} from "react-router-dom"
+import { useNavigate, Form, useActionData} from "react-router-dom"
 import Formulario from "../components/Formulario"
+import Error from "../components/Error"
+export  async function action({request}) {
+  const formData = await request.formData()
+  const datos = Object.fromEntries(formData)
+  
+  //validacion
+  const errores = []
 
-export  async function action() {
-  console.log("desde accion")
-  return null
+  if(Object.values(datos).includes("")) {
+    errores.push("Todos los campos son obligatorio")
+  }
+
+  //validacion de email
+  let regex = new RegExp("([!#-'*+/-9=?A-Z^-~-]+(\.[!#-'*+/-9=?A-Z^-~-]+)*|\"\(\[\]!#-[^-~ \t]|(\\[\t -~]))+\")@([!#-'*+/-9=?A-Z^-~-]+(\.[!#-'*+/-9=?A-Z^-~-]+)*|\[[\t -Z^-~]*])");
+  if(!regex.test("email")){
+    errores.push("El Email no es valido")
+  }
+  
+  //retorna si hay errores
+  if(Object.keys(errores).length) {
+    return errores
+  }
 }
 
 
 function NuevoCliente() {
   const navigate = useNavigate()
+  const errores = useActionData()
   return (
     <>
       <h1 className="font-black text-4xl text-blue-900">Nuevo Cliente</h1>
@@ -23,8 +42,12 @@ function NuevoCliente() {
         </button>
       </div>
       <div className="bg-white shadow rounded-md md:w-3/4 mx-auto px-5 py-10 mt-20">
+        
+        {errores?.length && errores.map( ( error, i) => <Error key={i}>{error}</Error> )}
+        
         <Form
           method="post"
+          noValidate
         >
           <Formulario />
 
